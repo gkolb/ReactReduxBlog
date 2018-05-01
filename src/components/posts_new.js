b7da195d823d37dae72b061df90e4ca729a535f3
gkolb <gkolb100@gmail.com>
2018-04-30 19:27:01 -0600

    Add form validation for title categories and content

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,28 +2,63 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-  renderTitleField(field) {
+  renderField(field) {
     return (
-      <div>
+      <div className="form-group">
+        <label>{field.label}</label>
         <input
+          clssName="form-control"
           type="text"
+          //spread operator that connects different actions in field to state
           {...field.input}
         />
+        {/*connect error object to name on <Field /> they must match!*/}
+        {field.meta.error}
       </div>
-    )
+    );
   }
+
   render() {
     return (
       <form>
         <Field
+          label="Title for Post"
           name="title"
-          component={this.renderTitleField}
+          component={this.renderField}
         />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+        <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     )
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = "Enter a title!"
+  }
+  if (!values.categories) {
+    errors.categories = "Enter a title!"
+  }
+  if (!values.content) {
+    errors.content = "Enter a title!"
+  }
+
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
